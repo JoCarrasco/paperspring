8a81318579ec16030a0e9af7844cf171278cd51b
Jo Carrasco <jogregcarrasco@gmail.com>
2024-10-14 20:35:33 -0400

    Install Spring boilerplate

---
+++ src/test/java/com/paperspring/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paperspring.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
